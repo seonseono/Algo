71fd7e8d5766f08bfd590383683994060454b60e
Seono.Lee <115915544+seonseono@users.noreply.github.com>
2024-03-27 09:29:08 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,5 +1,7 @@
-n = int(input())
-n_list = list(map(int, input().split()))
-v = int(input())
+import sys
+
+n = int(sys.stdin.readline())
+n_list = list(map(int, sys.stdin.readline().split()))
+v = int(sys.stdin.readline())
 
 print(n_list.count(v))
