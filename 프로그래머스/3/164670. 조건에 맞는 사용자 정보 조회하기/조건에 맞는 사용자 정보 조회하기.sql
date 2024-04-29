d18d25661ad00e6a29907bc16e56d1de4c963f20
Seono.Lee <115915544+seonseono@users.noreply.github.com>
2024-04-29 15:50:17 +0900

    Update 조건에 맞는 사용자 정보 조회하기.sql
    Alias modify

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 SELECT USER_ID, NICKNAME, 
         CONCAT(city,' ',street_address1,' ',street_address2) AS 전체주소, 
-        CONCAT(substr(TLNO,1,3), '-', substr(TLNO,4,4), '-', substr(TLNO,8,4))          AS 전화번호
+        CONCAT(substr(TLNO,1,3), '-', substr(TLNO,4,4), '-', substr(TLNO,8,4)) AS 전화번호
 FROM used_goods_board AS b, 
      used_goods_user AS u
 WHERE writer_id = user_id
